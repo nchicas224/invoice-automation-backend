77503af4976d173745cccfc11b70352c35685545
nchicas224 <armando2095@hotmail.com>
2025-05-22 15:41:33 -0400

    Remove: raw inbox id from src: Add: env variable for it

--- function_app.py
+++ function_app.py
@@ -35,7 +35,7 @@ async def notify_new_mail(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(req.params["validationToken"], status_code=200)
     
     initialize_logger()
-    
+
     #Validate ClientState
     try:
         body = req.get_json()
@@ -96,7 +96,7 @@ async def renew_subscription(timer: func.TimerRequest) -> None:
     request_body = Subscription(
         change_type = "created",
         notification_url= "https://invoice-automation-app-staging.azurewebsites.net/api/NotifyNewMail",
-        resource= "users/da59e8e3-205e-40c1-9059-238b64e791e6/mailFolders('Inbox')/messages",
+        resource= f"users/{os.environ["INVOICES_MAILBOX_ID"]}/mailFolders('Inbox')/messages",
         expiration_date_time= next_expiry,
         client_state= client_state,
         latest_supported_tls_version= "v1_2"
