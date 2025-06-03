412da95c662e99a5be48a99c13393fb73198b262
nchicas224 <armando2095@hotmail.com>
2025-06-03 16:30:58 -0400

    Test: Fix logging for existing.

--- function_app.py
+++ function_app.py
@@ -112,7 +112,7 @@ async def notify_new_mail(req: func.HttpRequest, starter: df.DurableOrchestratio
                 return func.HttpResponse(status_code=200)
         else:
             logging.info(f"Instance ({instance_id}) already in progress: {existing}")
-            logging.info(f"Status: {existing}")
+            logging.info(f"Status: {existing.custom_status} -> ID: {existing.instance_id}")
 
     # Return Accepted -> Processing status to Graph API
     logging.info("Notification completed. Returning status: 'OK'")
