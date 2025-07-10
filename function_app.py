0f7d5b45c3cad7fcff8bc07183c4db2038531acf
nchicas224 <armando2095@hotmail.com>
2025-07-10 18:52:20 -0400

    Fix: dfc closing error

--- function_app.py
+++ function_app.py
@@ -184,6 +184,7 @@ async def ping_wake_timer(timer: func.TimerRequest) -> None:
         logging.info("Pinged Notify new mail on: COLD START")
         pingRec.raise_for_status()
         await pingRec.aclose()
+    creds.close()
     _cold_start = False
     logging.info("Pinged succeeded! Host is now warm.")
 
